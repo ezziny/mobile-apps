317ab022962cc474dccc36eb67a505320753a8f8
ezziny <ezzinynone@gmail.com>
2024-09-29 18:00:01 +0300

    I'm done with ts

--- FlyingText.swift
+++ FlyingText.swift
@@ -1,5 +1,5 @@
 //
-//  FlyingText.swift
+//  SwiftUIView.swift
 //  Set game
 //
 //  Created by ezz on 29/09/2024.
@@ -8,11 +8,32 @@
 import SwiftUI
 
 struct FlyingText: View {
+    let text: String
+    let textColor: Color
+    
+    @State private var offset: CGFloat = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(text)
+            .font(.system(size: 25))
+            .foregroundColor(textColor)
+            .shadow(color: .black, radius: 1.5, x: 1, y: 1)
+            .offset(x: 0, y: offset)
+            .opacity(offset != 0 ? 0 : 1)
+            .onAppear {
+                withAnimation(.easeIn(duration: 1.5)) {
+                    offset = textColor == .red ? 200 : -200
+                }
+            }
+            .onDisappear {
+                offset = 0
+            }
     }
 }
 
-#Preview {
-    FlyingText()
+struct FlyingText_Previews: PreviewProvider {
+    static var previews: some View {
+        FlyingText(text: "Mismatch ❌", textColor: .red)
+    }
 }
+//Too busy/lazy to implement this but hey mr stalker. it's a good addition if you wanna use it.
