a61eb8a30e7338eb791480b27a20f9fbdbb43745
ezziny <ezzinynone@gmail.com>
2024-09-29 17:58:37 +0300

    I'm done with ts

---
+++ FlyingText.swift
@@ -0,0 +1,18 @@
+//
+//  FlyingText.swift
+//  Set game
+//
+//  Created by ezz on 29/09/2024.
+//
+
+import SwiftUI
+
+struct FlyingText: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlyingText()
+}
